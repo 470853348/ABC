09cdaff309c95e265a59076639fc4a8c98fd3e11
wangfw <wangfw@wangfwdeMac-mini.local>
2018-03-29 14:36:54 +0800

    init

---
+++ ABC/AbcModel.h
@@ -0,0 +1,13 @@
+//
+//  AbcModel.h
+//  Demo
+//
+//  Created by wangfw on 2018/3/29.
+//  Copyright © 2018年 澳视. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AbcModel : NSObject
+
+@end
---
+++ ABC/AbcModel.m
@@ -0,0 +1,13 @@
+//
+//  AbcModel.m
+//  Demo
+//
+//  Created by wangfw on 2018/3/29.
+//  Copyright © 2018年 澳视. All rights reserved.
+//
+
+#import "AbcModel.h"
+
+@implementation AbcModel
+
+@end
